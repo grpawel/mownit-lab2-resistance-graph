1bfdd3362fd4fa8186299f8ebe4f5d25dc60b0a6
Pawel Grochola <grocholapawel@gmail.com>
2017-11-03 13:47:42 +0100

    Fix vertices not being added to graph

--- src/GraphReader.java
+++ src/GraphReader.java
@@ -17,12 +17,14 @@ public class GraphReader {
         while (scanner.hasNextLine()) {
             final String line = scanner.nextLine();
             final String[] splitLine = line.split(" ");
-            final String firstNode = splitLine[0];
-            final String secondNode = splitLine[1];
+            final String firstVertex = splitLine[0];
+            final String secondVertex = splitLine[1];
             final double value = Double.parseDouble(splitLine[2]);
             final Edge.Type type = scanner.hasNextLine() ? Edge.Type.RESISTANCE : Edge.Type.EMF;
             final Edge edge = new Edge(type, value);
-            graph.addEdge(firstNode, secondNode, edge);
+            graph.addVertex(firstVertex);
+            graph.addVertex(secondVertex);
+            graph.addEdge(firstVertex, secondVertex, edge);
         }
         return graph;
     }
