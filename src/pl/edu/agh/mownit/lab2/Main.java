6170db26059f3d1ec80ebd1db013f031e665dc30
Pawel Grochola <grocholapawel@gmail.com>
2017-11-06 03:37:00 +0100

    Hook up GraphViz to visualize graph

--- src/pl/edu/agh/mownit/lab2/Main.java
+++ src/pl/edu/agh/mownit/lab2/Main.java
@@ -1,13 +1,15 @@
 package pl.edu.agh.mownit.lab2;
 
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Created by Paweł Grochola on 03.11.2017.
  */
 public class Main {
-    public static void main(final String[] args) throws FileNotFoundException {
-        if(args.length != 1) {
+    public static void main(final String[] args) throws IOException, InterruptedException {
+        if (args.length != 1) {
             System.out.println("Please specify file with graph in first argument");
             return;
         }
@@ -17,6 +19,25 @@ public class Main {
         new GraphSolver(equations, graph).applyKirchoffLaws();
         equations.solve();
         final String graphString = new GraphSaver(graph, equations.getResult()).save();
-        System.out.println(graphString);
+        generateGraphvizOutput(graphString, args[0]);
+    }
+
+    private static void generateGraphvizOutput(final String graphString, final String fileName) throws IOException, InterruptedException {
+        final File tempFile = File.createTempFile(java.util.UUID.randomUUID().toString(), ".dot");
+        Files.write(Paths.get(tempFile.getAbsolutePath()), graphString.getBytes());
+        final String fileNameWithoutExtension = new File(fileName).getAbsolutePath().replaceFirst("[.][^.]+$", "");
+        final Process process = new ProcessBuilder(
+                "neato",
+                tempFile.getAbsolutePath(),
+                "-Tpng",
+                "-o" + fileNameWithoutExtension + ".png").start();
+
+        final InputStream is = process.getErrorStream();
+        final InputStreamReader isr = new InputStreamReader(is);
+        final BufferedReader br = new BufferedReader(isr);
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
     }
 }
